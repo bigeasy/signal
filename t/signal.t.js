578a5c55c970957e74546399759d43263e169806
Alan Gutierrez <alan@prettyrobots.com>
2018-01-29 22:58:54 -0600

    Rename `assert` to `okay`.

--- t/signal.t.js
+++ t/signal.t.js
@@ -1,36 +1,36 @@
 require('proof')(8, prove)
 
-function prove (assert) {
+function prove (okay) {
     var Signal = require('..')
     var signal = new Signal
     var cookie = signal.wait(function () { throw new Error })
     signal.wait(function (error, one, two, three) {
-        assert([ one, two, three ], [ 1, 2, 3 ], 'called')
+        okay([ one, two, three ], [ 1, 2, 3 ], 'called')
     })
     signal.cancel(cookie)
     signal.cancel(cookie)
     signal.notify(null, 1, 2, 3)
     signal.wait(100, function (error, completed) {
-        assert(completed, 'timeout canceled')
+        okay(completed, 'timeout canceled')
     })
     signal.notify(null, true)
-    var cookie = signal.wait(1, function () { assert(true, 'timer canceled') })
+    var cookie = signal.wait(1, function () { okay(true, 'timer canceled') })
     signal.cancel(cookie)()
     signal.wait(function (error, value) {
-        assert(value, 1, 'unlatch')
+        okay(value, 1, 'unlatch')
     })
     signal.unlatch(null, 1)
     signal.unlatch()
     signal.wait(function (error, value) {
-        assert(value, 1, 'open')
+        okay(value, 1, 'open')
     })
     signal = new Signal
     signal.wait(function (error, value) {
-        assert(value, 2, 'notify will cancel')
+        okay(value, 2, 'notify will cancel')
         signal.cancel(cancel)(null, value * 2)
     })
     var cancel = signal.wait(function (error, value) {
-        assert(value, 4, 'canceled during notify')
+        okay(value, 4, 'canceled during notify')
     })
     signal.notify(null, 2)
 
@@ -38,7 +38,7 @@ function prove (assert) {
     var second = new Signal
 
     Signal.first(first, second, function (error, value) {
-        assert(value, 2, 'second')
+        okay(value, 2, 'second')
     })
     second.unlatch(null, 2)
     first.unlatch(null, 1)
