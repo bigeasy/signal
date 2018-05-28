88d60bd4c7ff8842175cede199c2c66298bc1e51
Alan Gutierrez <alan@prettyrobots.com>
2018-05-28 00:34:00 -0500

    Add some Docco.

--- signal.js
+++ signal.js
@@ -51,11 +51,24 @@ Signal.prototype.cancel = function (cookie) {
     return left.callback
 }
 
+// Notify listening waits.
+
+//
 Signal.prototype.notify = function () {
     var vargs = Array.prototype.slice.call(arguments)
+
+    // Dubious. No real use for it. Plus, it might as well be a count.
     this.occupied = false
+
+    // We shift a new array into waiting so that a notified function can wait on
+    // a subsequent notification. We do not pop it or replace it because we want
+    // our cancel function above to be able to find it and cancel it. We're
+    // going to want to be able cancel both exiting waits and waits added during
+    // the notification.
     var waiting = this._waiting[0]
     this._waiting.unshift([])
+
+    // We shift first so we don't wreck the array if a wait tries to itself.
     while (waiting.length != 0) {
         var waited = waiting.shift()
         if (waited.timeout !== null) {
@@ -63,11 +76,18 @@ Signal.prototype.notify = function () {
         }
         waited.callback.apply(null, vargs)
     }
+
+    // Do not understand why this is necessary. Why can't we just pop?
     this._waiting = this._waiting.filter(function (waiting, index) {
+        console.log(waiting.length)
         return index == 0 || waiting.length > 0
     })
 }
 
+// Notify listening waits with arguments that will be immediately given to any
+// subsequent waits.
+
+//
 Signal.prototype.unlatch = function () {
     if (this.open == null) {
         this.notify.apply(this, this.open = Array.prototype.slice.call(arguments))
